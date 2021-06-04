fa872e947496985c22321ff37ef6d656c29f4d58
Freeman-md <freemanmadudili@gmail.com>
2021-06-04 22:32:53 +0100

    Logout & Navbar Auth Links

--- accounts/views.py
+++ accounts/views.py
@@ -26,10 +26,6 @@ def register(request):
                 else:
                     # Looks good
                     user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
-                    # Login after register
-                    # auth.login(request, user)
-                    # messages.success(request, 'You are now logged in')
-                    # return redirect('index')
                     user.save()
                     messages.success(request, 'You are now registered and can now log in')
                     return redirect('login')
@@ -60,6 +56,9 @@ def login(request):
 
 
 def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are nw logged out')
     return redirect('index')
 
 
